c8c85bb8dbeb27a70052057aa9467fd5a0fd6dce
Nata-code-project <l.a.1606l.a@gmail.com>
2026-01-05 21:54:54 +0200

    add lesson 19

--- lesson_19/js/script.js
+++ lesson_19/js/script.js
@@ -7,3 +7,16 @@ window.addEventListener("scroll", function () {
     header.classList.remove('header--scrolled');
   }
 });
+
+"use strict"
+
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+	const targetElement = e.target
+
+	if (targetElement.closest('.header__icon-menu')) {
+		document.documentElement.toggleAttribute('data-menu-open')
+	}
+
+}
